acf2c4a3f3f1fa948f3466a47115c80183030b6b
roykarazarwe. <leroykendal@gmail.com.>
2024-05-07 17:13:18 +0300

    Packages

--- Screens/VideoScreen.js
+++ Screens/VideoScreen.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 //import YoutubeIframe from 'react-native-youtube-iframe'; 
-import YoutubePlayer from 'react-native-youtube-iframe';  // install gesureHandler v2.14.0,  webview v11  npx expo install react-native-reanimated->to solve main Application error
+import YoutubePlayer from 'react-native-youtube-iframe';  // install react-native-gesture-handler v2.14.0,  webview v11  npx expo install react-native-reanimated->to solve main Application error
 
 function VideoScreen(props) {
     const [video, setVideo] = useState(null);
